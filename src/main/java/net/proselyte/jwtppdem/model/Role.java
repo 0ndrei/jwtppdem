3cdc504342afa5e6690f40277eb5b04da75b4c26
0ndrei <lungu.andrei.00@mail.ru>
2021-10-11 12:51:26 +0300

    Create table for db

--- src/main/java/net/proselyte/jwtppdem/model/Role.java
+++ src/main/java/net/proselyte/jwtppdem/model/Role.java
@@ -2,9 +2,7 @@ package net.proselyte.jwtppdem.model;
 
 import lombok.Data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
@@ -14,5 +12,6 @@ public class Role extends BaseEntity{
     @Column(name = "name")
     private String name;
 
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
     private List<User> users;
 }
