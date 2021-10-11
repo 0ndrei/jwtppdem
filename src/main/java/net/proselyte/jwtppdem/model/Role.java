1b31495f8884dc0a40cb7823d6cd49fc658226c0
0ndrei <lungu.andrei.00@mail.ru>
2021-10-11 12:33:19 +0300

    Add class Role and User for package model

--- src/main/java/net/proselyte/jwtppdem/model/Role.java
+++ src/main/java/net/proselyte/jwtppdem/model/Role.java
@@ -1,4 +1,18 @@
 package net.proselyte.jwtppdem.model;
 
-public class Role {
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role extends BaseEntity{
+    @Column(name = "name")
+    private String name;
+
+    private List<User> users;
 }
