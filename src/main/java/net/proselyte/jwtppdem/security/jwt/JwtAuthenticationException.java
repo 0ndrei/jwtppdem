30683a6716da943565c00e417f139f3d91c4d833
0ndrei <lungu.andrei.00@mail.ru>
2021-10-15 19:18:30 +0300

    Add class JwtAuthenticationException

---
+++ src/main/java/net/proselyte/jwtppdem/security/jwt/JwtAuthenticationException.java
@@ -0,0 +1,13 @@
+package net.proselyte.jwtppdem.security.jwt;
+
+import javax.naming.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+    public JwtAuthenticationException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+    }
+}
